08870c6339761f172405c733aa81d6ce4f8c8766
Rui Andrade <ruiandrade@outlook.com>
2017-06-07 16:29:11 +0100

    header file describing class

---
+++ LEDProgress.h
@@ -0,0 +1,28 @@
+/*
+  LEDProgress.h - Library for simulating progress indication with LEDs
+  Created by Rui Andrade, June 2017.
+  Released into the public domain.
+*/
+
+#ifndef LEDProgress_h
+#define LEDProgress_h
+
+#include "Arduino.h"
+
+class LEDProgress
+{
+  public:
+   	LEDProgress(int leds[], int nleds, int maxprog);
+    void setProgress(int prog);
+    int getProgress();
+
+  private:
+    int* _leds; // led pins
+    int _nleds; // number of leds
+    int _maxprog; // maximum progress in total
+    int _prog; // current progress
+    int* _buckets; // fade values for each led
+    int _ledprog; // how much progress each led represents
+};
+
+#endif
